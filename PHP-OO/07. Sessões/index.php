3540822edb658811a0c29fc4e051d8e845336977
kauan <kauansbflu@gmail.com>
2024-11-11 12:22:10 -0300

    sessões finalizada

--- PHP-OO/07. Sessões/index.php
+++ PHP-OO/07. Sessões/index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./assets/css/index.css">
     <title>Bem-vindo ao Sistema</title>
 </head>
@@ -15,7 +14,7 @@
     <p>Para continuar, por favor faça o login.</p>
     <form action="views/login.php" method="get">
         <button type="submit" class="btn-login">
-            <i class="fa-solid fa-user"></i> Login
+            Login
         </button>
     </form>
 </div>
--- PHP-OO/07. Sessões/views/login.php
+++ PHP-OO/07. Sessões/views/login.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../assets/css/style.css">
 
     <title>Login</title>
@@ -18,7 +17,6 @@ session_start();
     <form id="login_form" method="post" action="../classes/AutenticaUsuario.php">
         <div id="form_header">
             <img src="../assets/img/logo.png" alt="Versátil TI">
-            <i id="mode_icon" class="fa-solid fa-moon"></i>
         </div>
 
         <!-- Div para exibição de erro -->
@@ -28,7 +26,6 @@ session_start();
             <div class="input-box">
                 <label for="name">Nome
                     <div class="input-field">
-                        <i class="fa-solid fa-user"></i>
                         <input type="text" id="login" name="login" required>
                     </div>
                 </label>
@@ -36,7 +33,6 @@ session_start();
             <div class="input-box">
                 <label for="senha">Senha
                     <div class="input-field">
-                        <i class="fa-solid fa-key"></i>
                         <input type="password" id="senha" name="senha" required>
                     </div>
                 </label>
