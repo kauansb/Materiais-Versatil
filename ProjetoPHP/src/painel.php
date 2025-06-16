28db4a4b0b8520b0cafb30c642b9217eadeb2dc4
kauan <kauansbflu@gmail.com>
2025-06-16 12:39:00 -0300

    refactor: Add viewport and X-UA-Compatible meta tags for responsive design in multiple files

--- ProjetoPHP/src/painel.php
+++ ProjetoPHP/src/painel.php
@@ -18,6 +18,8 @@ $msg = $_SESSION['msg'] ?? '';
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Cadastro de Clientes</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../assets/css/main.css">
